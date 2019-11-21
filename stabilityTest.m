e608d18c2cb7ba1db1dda4b76c1cce50441cf17d
Dai Luyao <dailuyaoME@foxmail.com>
2019-11-21 14:57:25 +0800

    revise stability test script

--- stabilityTest.m
+++ stabilityTest.m
@@ -1,20 +1,17 @@
-% %%
-% sys1 = feedback(GcDis*GpDis,1);
-% sys2 = feedback(GcDis*GpDis*2,1);
-% [num1,den1] = tfdata(sys1,'v');
-% [num2,den2] = tfdata(sys2,'v');
-% [A1,B1,C1,D1] = tf2ss(num1,den1);
-% [A2,B2,C2,D2] = tf2ss(num2,den2);
-% %%
-% n = size(A1,1);
-% cvx_begin sdp
-% variable P(n,n) symmetric
-% A1'*P*A1 - P <= zeros(n)
-% A2'*P*A2 - P <= zeros(n)
-% P >= zeros(n)
-% cvx_end
+%% stability analysis under discrete time domain using cvx
+sys1 = feedback(GcDis*GpDis,1);
+sys2 = feedback(GcDis*GpDis*1.1,1);
+[A1,B1,C1,D1] = ssdata(sys1);
+[A2,B2,C2,D2] = ssdata(sys2);
+n = size(A1,1);
+cvx_begin sdp
+variable P(n,n) symmetric
+A1'*P*A1 - P <= eye(n)
+A2'*P*A2 - P <= eye(n)
+P >= eye(n)
+cvx_end
 
-%% continuous time domain
+%% stability analysis under continuous time domain using cvx
 
 sys1 = ss(minreal(feedback(C*Gp,1)));
 sys2 = ss(minreal(feedback(C*Gp*1.1,1)));
@@ -30,15 +27,12 @@ A1'*P + P*A1 < eye(n)
 A2'*P + P*A2 < eye(n)
 P > eye(n)
 cvx_end
-%%
-%% continuous time domain
+%% stability analysis under continuous time domain using LMI toolbox
 
 sys1 = minreal(feedback(C*Gp,1));
 sys2 = minreal(feedback(C*Gp*1.5,1));
-[num1,den1] = tfdata(sys1,'v');
-[num2,den2] = tfdata(sys2,'v');
-[A1,B1,C1,D1] = tf2ss(num1,den1);
-[A2,B2,C2,D2] = tf2ss(num2,den2);
+[A1,B1,C1,D1] = ssdata(sys1);
+[A2,B2,C2,D2] = ssdata(sys2);
 
 n = size(A1,1);
 setlmis([]) 
@@ -50,15 +44,12 @@ lmis = getlmis;
 [tmin,xfeas] = feasp(lmis);
 P = dec2mat(lmis,xfeas,p);
 
-%%
-%% continuous time domain
+%% stability analysis under discrete time domain using LMI toolbox
 
 sys1 = minreal(feedback(C*Gp,1));
 sys2 = minreal(feedback(C*Gp*1.1,1));
-[num1,den1] = tfdata(sys1,'v');
-[num2,den2] = tfdata(sys2,'v');
-[A1,B1,C1,D1] = tf2ss(num1,den1);
-[A2,B2,C2,D2] = tf2ss(num2,den2);
+[A1,B1,C1,D1] = ssdata(sys1);
+[A2,B2,C2,D2] = ssdata(sys2);
 
 n = size(A1,1);
 setlmis([]) 
